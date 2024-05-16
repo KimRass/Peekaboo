7371783c85f730402112c86d3dc5dd87353065c6
KimRass <rmx1000@yonsei.ac.kr>
2024-05-17 01:17:58 +0900

    torch.no_grad()?

--- peekaboo.py
+++ peekaboo.py
@@ -13,7 +13,7 @@ from PIL import Image
 import random
 from tqdm import tqdm
 
-from utils import unload_from_gpu, denorm
+from utils import unload_from_gpu, image_to_grid
 
 
 class LearnableAlphaMask(nn.Module):
@@ -21,6 +21,7 @@ class LearnableAlphaMask(nn.Module):
     "We use a randomly initialized alpha mask ($\alpha$) to alpha-blend the
     puppy image ($x$) with different backgrounds ($b$), generating new composite
     images ($\hat{x}$): $\hat{x} = \alpha x + (1 - \alpha)b$"
+    "At the initial iteration it is entirely random Gaussian noise."
     """
     def __init__(self, h, w):
         super().__init__()
@@ -28,52 +29,22 @@ class LearnableAlphaMask(nn.Module):
         self.alpha = nn.Parameter(torch.randn(h, w))
 
     def forward(self, channels):
-        return torch.sigmoid(
-            einops.repeat(self.alpha, pattern="h w -> 1 c h w", c=channels),
+        """
+        "We apply a bilateral blur to that matrix and then clip it between 0
+        and 1 using a sigmoid function."
+        """
+        return einops.repeat(
+            torch.sigmoid(self.alpha), pattern="h w -> 1 c h w", c=channels,
         )
-
-
-def get_rand_uint8_color():
-    return (
-        random.randint(0, 255), random.randint(0, 255), random.randint(0, 255),
-    )
-
-
-def get_rand_uint8_color_pil_image(h, w):
-    return Image.new("RGB", size=(w, h), color=get_rand_uint8_color())
 
 
 class Peekaboo(nn.Module):
-    """
-    "We first degrade latent vector $z$ of composite image $x$ using forward
-    diffusion, introducing Gaussian noise $\epsilon \sim N$ to $z$, resulting in
-    a noisy $\tilde{z}$. We then perform diffusion denoising conditioned on the
-    text embedding with pre-trained $\mathcal{D}$. Our loss $\mathcal{L}_{s}$ is measured
-    as the reconstruction error of noise $\epsilon$, given noisy $\tilde{z}$ and
-    text embedding $\mathcal{T}(p)$: $\mathcal{L}_{s}
-    = \text{MSE}(\epsilon, \mathcal{D}(\tilde{z}, \mathcal{T}(p)))$ where
-    $\text{MSE}$ refers to mean-squared loss."
-    """
-    def compose(self):
-        image = Image.open(self.img_path).convert("RGB")
-        image = self.transform(image)
-        image = self.to_tensor_and_norm(image)[None, ...]
-
-        _, _, h, w = image.shape
-        bg = get_rand_uint8_color_pil_image(h=h, w=w)
-        bg = self.to_tensor_and_norm(bg)[None, ...]
-        
-        return torch.lerp(
-            image, bg, self.alpha_mask(channels=3),
-        )
-
-    def __init__(self, sd, img_path, prompt):
+    def __init__(self, sd):
         super().__init__()
 
         self.sd = sd
-        self.img_path = img_path
-        self.prompt = prompt
-
+        self.sd.vae.eval()
+        self.sd.unet.eval()
 
         self.img_size = sd.unet.config.sample_size * sd.vae_scale_factor
         self.alpha_mask = LearnableAlphaMask(h=self.img_size, w=self.img_size)
@@ -89,23 +60,6 @@ class Peekaboo(nn.Module):
                 T.Normalize(mean=(0.5, 0.5, 0.5), std=(0.5, 0.5, 0.5)),
             ]
         )
-        self.comp_image = self.compose()
-
-        self.prompt_embed = self.get_prompt_embed()
-
-    def get_prompt_embed(self):
-        prompt_embed, _ = self.sd.encode_prompt( # "$\mathcal{T}(p)$"
-            prompt=self.prompt, # "$p$"
-            device=self.sd.device,
-            num_images_per_prompt=1,
-            do_classifier_free_guidance=False,
-            negative_prompt=None,
-            prompt_embeds=None,
-            negative_prompt_embeds=None,
-            lora_scale=None,
-            clip_skip=None,
-        )
-        return prompt_embed
 
     def get_alpha_reg_loss(self):
         """
@@ -114,10 +68,22 @@ class Peekaboo(nn.Module):
         """
         return torch.sum(self.alpha_mask.alpha, dim=(-2, -1))
 
-    def get_score_distil_loss(self):
-        comp_image_latent = self.sd.vae.encode(
-            self.comp_image.to(dtype=sd.dtype, device=self.sd.device),
-        ).latent_dist.mode()# "$z$"
+    def get_score_distil_loss(self, comp_image, prompt_embed):
+        """
+        "We first degrade latent vector $z$ of composite image $x$ using forward
+        diffusion, introducing Gaussian noise $\epsilon \sim N$ to $z$,
+        resulting in a noisy $\tilde{z}$. We then perform diffusion denoising
+        conditioned on the text embedding with pre-trained $\mathcal{D}$. Our
+        loss $\mathcal{L}_{s}$ is measured as the reconstruction error of noise
+        $\epsilon$, given noisy $\tilde{z}$ and text embedding
+        $\mathcal{T}(p)$: $\mathcal{L}_{s}
+        = \text{MSE}(\epsilon, \mathcal{D}(\tilde{z}, \mathcal{T}(p)))$ where
+        $\text{MSE}$ refers to mean-squared loss."
+        """
+        with torch.no_grad():
+            comp_image_latent = self.sd.vae.encode(
+                comp_image.to(dtype=sd.dtype, device=self.sd.device),
+            ).latent_dist.mode() # "$z$"
         rand_noise = self.sd.prepare_latents(
             batch_size=1,
             num_channels_latents=self.sd.unet.config.in_channels,
@@ -135,31 +101,72 @@ class Peekaboo(nn.Module):
             original_samples=comp_image_latent,
             noise=rand_noise,
             timesteps=rand_timestep,
-        )
-
+        ) # "$\tilde{z}$"
+        # with torch.inference_mode():
         with torch.no_grad():
             pred_noise = self.sd.unet(
                 sample=noisy_comp_image_latent,
                 timestep=rand_timestep,
-                encoder_hidden_states=self.prompt_embed,
+                encoder_hidden_states=prompt_embed,
                 timestep_cond=None,
                 cross_attention_kwargs=None,
                 added_cond_kwargs=None,
                 return_dict=False,
-            )[0] # "$\mathcal{D}$"
+            )[0] # "$\hat{\epsilon}$"
         return torch.sum((rand_noise - pred_noise) ** 2)
 
-    def get_loss(self):
-        return self.get_alpha_reg_loss() + self.get_score_distil_loss()
+    @staticmethod
+    def get_rand_uint8_color_bg(h, w):
+        return Image.new(
+            mode="RGB",
+            size=(w, h),
+            color=(
+                random.randint(0, 255),
+                random.randint(0, 255),
+                random.randint(0, 255),
+            ),
+        )
 
-    def optimize(self, num_steps, lr):
+    def get_image(self, img_path):
+        image = Image.open(img_path).convert("RGB")
+        image = self.transform(image)
+        return self.to_tensor_and_norm(image)[None, ...]
+
+    # @torch.inference_mode()
+    @torch.no_grad()
+    def get_prompt_embed(self, prompt):
+        prompt_embed, _ = self.sd.encode_prompt( # "$\mathcal{T}(p)$"
+            prompt=prompt, # "$p$"
+            device=self.sd.device,
+            num_images_per_prompt=1,
+            do_classifier_free_guidance=False,
+            negative_prompt=None,
+            prompt_embeds=None,
+            negative_prompt_embeds=None,
+            lora_scale=None,
+            clip_skip=None,
+        )
+        return prompt_embed.detach()
+
+    def get_composite_image(self, image):
+        _, _, h, w = image.shape
+        bg = self.get_rand_uint8_color_bg(h=h, w=w)
+        bg = self.to_tensor_and_norm(bg)[None, ...]        
+        return torch.lerp(image, bg, self.alpha_mask(channels=3))
+
+    def optimize(self, img_path, prompt, num_steps, lr):
+        image = self.get_image(img_path)
+        prompt_embed = self.get_prompt_embed(prompt)
+
         optim = SGD(self.alpha_mask.parameters(), lr=lr)
         for _ in tqdm(range(num_steps)):
-            # loss = self.get_loss()
             reg_loss = self.get_alpha_reg_loss()
-            score_distil_loss = self.get_score_distil_loss()
+            comp_image = self.get_composite_image(image)
+            score_distil_loss = self.get_score_distil_loss(
+                comp_image=comp_image, prompt_embed=prompt_embed,
+            )
             loss = reg_loss + score_distil_loss
-            # loss = reg_loss
+
             log = f"[ {reg_loss.item():.3f} ]"
             log += f"[ {score_distil_loss.item():.3f} ]"
             log += f"[ {loss.item():.3f} ]"
@@ -168,7 +175,8 @@ class Peekaboo(nn.Module):
             optim.zero_grad()
             loss.backward()
             optim.step()
-        return self.alpha_mask.alpha
+        # return self.alpha_mask.alpha
+        return self.get_composite_image(image)
 
 
 if __name__ == "__main__":
@@ -181,11 +189,13 @@ if __name__ == "__main__":
     ).to(device)
     # unload_from_gpu(sd)
 
+    peekaboo = Peekaboo(sd)
     img_path = "/Users/jongbeomkim/Desktop/workspace/Peekaboo/resources/harry_potter.webp"
     prompt = "Harry Potter"
-    peekaboo = Peekaboo(sd=sd, img_path=img_path, prompt=prompt)
-    
-    alpha = peekaboo.optimize(num_steps=50, lr=0.00001)
+    comp_image = peekaboo.optimize(
+        img_path=img_path, prompt=prompt, num_steps=50, lr=0.00001,
+    )
+    image_to_grid(comp_image, 1).show()
 
-    temp = torch.sigmoid(peekaboo.alpha_mask.alpha)
-    TF.to_pil_image(temp).show()
+    # temp = torch.sigmoid(peekaboo.alpha_mask.alpha)
+    # TF.to_pil_image(temp).show()
--- utils.py
+++ utils.py
@@ -1,5 +1,6 @@
 import torch
 import torchvision.transforms.functional as TF
+from torchvision.utils import make_grid
 import numpy as np
 import gc
 
@@ -14,3 +15,12 @@ def denorm(x, mean=(0.5, 0.5, 0.5), std=(0.5, 0.5, 0.5)):
     return TF.normalize(
         x, mean=-(np.array(mean) / np.array(std)), std=(1 / np.array(std)),
     )
+
+
+def image_to_grid(image, n_cols):
+    tensor = image.clone().detach().cpu()
+    tensor = denorm(tensor)
+    grid = make_grid(tensor, nrow=n_cols, padding=1, pad_value=1)
+    grid.clamp_(0, 1)
+    grid = TF.to_pil_image(grid)
+    return grid
